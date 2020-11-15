716e0e0738ae6ed763da7ff7a387b36b4629aa4a
TTorgersen <tori.torgersen@hotmail.com>
2020-11-15 12:47:18 +0100

    landing page

---
+++ src/components/landingpage.vue
@@ -0,0 +1,20 @@
+<template> 
+<div>
+    <header class="header-1">Undergoing construction. </header>
+    <img src ="../assets/builder2.png"/>
+</div>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'Landing', 
+
+
+}
+</script>
+
+<style>
+
+</style>
